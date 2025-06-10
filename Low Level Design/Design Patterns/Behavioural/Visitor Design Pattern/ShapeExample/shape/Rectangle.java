e2ce5e90f6e4f77b91a96ad044c14bb6f8122388
Dhanashree Kamble <127020615+dhanashreesk@users.noreply.github.com>
2025-06-10 23:53:43 +0530

    Update Rectangle.java

--- Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/shape/Rectangle.java
+++ Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/shape/Rectangle.java
@@ -1,4 +1,28 @@
-package shape;
+package ShapeExample.shape;
 
-public class Rectangle {
+import ShapeExample.visitor.Visitor;
+
+public class Rectangle implements Shape{
+
+    private Double width;
+    private Double height;
+
+    public Rectangle (Double width , Double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public Double getWidth(){
+        return width;
+    }
+
+    public Double getHeight(){
+        return height;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visitRectange(this);
+    }
+
 }
