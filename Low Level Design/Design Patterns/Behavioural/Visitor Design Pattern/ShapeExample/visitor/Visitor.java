8a075befb5556afd252265b07153bf57576343fb
Dhanashree Kamble <127020615+dhanashreesk@users.noreply.github.com>
2025-06-10 23:56:09 +0530

    Update Visitor.java

--- Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/Visitor.java
+++ Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/Visitor.java
@@ -1,7 +1,11 @@
-package Visitor;
-import shape.Circle;
+package ShapeExample.visitor;
+import ShapeExample.shape.Circle;
+import ShapeExample.shape.Rectangle;
+import ShapeExample.shape.Square;
 public interface Visitor {
 
     void visitCircle(Circle circle);
-
+    void visitRectange(Rectangle rectangle);
+    void visitSquare(Square square);
+    
 }
