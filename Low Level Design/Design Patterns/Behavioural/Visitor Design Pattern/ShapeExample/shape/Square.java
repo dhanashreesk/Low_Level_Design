52dc3b4be542f9329eb20e07a3c8a9e5ac755e39
Dhanashree Kamble <127020615+dhanashreesk@users.noreply.github.com>
2025-06-10 23:54:30 +0530

    Update Square.java

--- Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/shape/Square.java
+++ Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/shape/Square.java
@@ -1,4 +1,21 @@
-package shape;
+package ShapeExample.shape;
 
-public class Square {
+import ShapeExample.visitor.Visitor;
+
+public class Square implements Shape {
+
+    private Double side;
+
+    public Square(Double side){
+        this.side = side;
+    }
+
+    public Double getSide(){
+        return side ; 
+    }
+
+    @Override
+    public void accept(Visitor visitor){
+        visitor.visitSquare(this);
+    }
 }
