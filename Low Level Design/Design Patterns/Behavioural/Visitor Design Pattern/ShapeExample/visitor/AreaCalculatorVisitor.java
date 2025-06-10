577b8e5d41b3eb8669cfee26512c18ccf3b1d778
Dhanashree Kamble <127020615+dhanashreesk@users.noreply.github.com>
2025-06-10 23:54:56 +0530

    Update AreaCalculatorVisitor.java

--- Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/AreaCalculatorVisitor.java
+++ Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/AreaCalculatorVisitor.java
@@ -1,4 +1,26 @@
-package Visitor;
+package ShapeExample.visitor;
 
-public class AreaCalculatorVisitor {
+import ShapeExample.shape.Circle;
+import ShapeExample.shape.Rectangle;
+import ShapeExample.shape.Square;
+
+public class AreaCalculatorVisitor implements Visitor {
+
+    @Override
+    public void visitCircle(Circle circle) {
+      Double area = Math.PI * circle.getRadius()*circle.getRadius();
+      System.out.println("Area of Circle :" + area);
+    }
+
+    @Override
+    public void visitRectange(Rectangle rectangle) {
+       Double area = rectangle.getHeight()*rectangle.getWidth();
+       System.out.println("Area of Rectangle : "+area);
+    }
+
+    @Override
+    public void visitSquare(Square square) {
+      Double area = square.getSide()*square.getSide();
+      System.out.println("Area of Square :" + area);
+    }
 }
