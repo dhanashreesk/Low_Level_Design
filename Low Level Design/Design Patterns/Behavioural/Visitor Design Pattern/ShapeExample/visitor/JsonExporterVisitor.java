9286c79ce491e34f04658cf591cc009707736182
Dhanashree Kamble <127020615+dhanashreesk@users.noreply.github.com>
2025-06-10 23:55:17 +0530

    Update JsonExporterVisitor.java

--- Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/JsonExporterVisitor.java
+++ Low Level Design/Design Patterns/Behavioural/Visitor Design Pattern/ShapeExample/visitor/JsonExporterVisitor.java
@@ -1,4 +1,23 @@
-package Visitor;
+package ShapeExample.visitor;
 
-public class JsonExporterVisitor {
+import ShapeExample.shape.Circle;
+import ShapeExample.shape.Rectangle;
+import ShapeExample.shape.Square;
+
+public class JsonExporterVisitor implements Visitor{
+
+    @Override
+    public void visitCircle(Circle circle) {
+        System.out.println( "{\"type\" : \"circle\" , \"radius\":" + circle.getRadius() + "}");
+    }
+
+    @Override
+    public void visitRectange(Rectangle rectangle) {
+        System.out.println( "{\"type\" : \"rectangle\" , \"width\":" + rectangle.getWidth() + ", \"height\": "+rectangle.getHeight() +"}");
+    }
+
+    @Override
+    public void visitSquare(Square square) {
+        System.out.println( "{\"type\" : \"square\" , \"side\":" + square.getSide() + "}");
+    }
 }
